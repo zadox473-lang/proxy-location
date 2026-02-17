5057271c47b7df640eeee763ac0dbf7eb2a21c0a
zadox473-lang <zadox473@gmail.com>
2026-02-17 13:40:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, C
 
 # --- CONFIGURATION ---
 TOKEN = "7888111866:AAH9wJOLVbdvTh6RwpyQLlS3VoJrjJWhzk4"
-SERVER_URL = "https://proxy-93ml.onrender.com"
+SERVER_URL = "https://proxy-location-1.onrender.com"
 
 # Force Join Channels (ID or Username)
 # Note: Public channels ke liye username aur private ke liye ID use hoti hai.
