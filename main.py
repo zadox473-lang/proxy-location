84126fbc8e83bdb4855bedf517d268b5342e294a
zadox473-lang <zadox473@gmail.com>
2026-02-17 13:44:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
 # --- CONFIGURATION ---
-TOKEN = "7888111866:AAH9wJOLVbdvTh6RwpyQLlS3VoJrjJWhzk4"
+TOKEN = "7888111866:AAG8ru6GppPhriXXuwtXhowS4ndpgvT7oD8"
 SERVER_URL = "https://proxy-location-1.onrender.com"
 
 # Force Join Channels (ID or Username)
